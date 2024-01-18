d8ead8b729005c10b064e8d8b0bd35c80f718bd9
mik8920 <daal.mikaela@gmail.com>
2024-01-18 11:36:52 +0200

    Add parentheses, change var choice and return to true or false

--- rps.js
+++ rps.js
@@ -1,5 +1,8 @@
+const rock = 'rock'
+const paper = 'paper'
+const scissors = 'scissors'
 
-const choice = [`rock`, `paper`, `scissors`]
+const choice = [rock, paper, scissors]
 
 const getComputerChoice = () => {
     const randomNumber = Math.floor(Math.random() * choice.length)
@@ -8,36 +11,40 @@ const getComputerChoice = () => {
 
 const gameRound = (playerSelection, computerSelection) => {
 
-    if (playerSelection==`rock` && computerSelection==choice[1] || playerSelection.toLowerCase() == "paper" && computerSelection == choice[2] || playerSelection.toLowerCase() == "scissors" && computerSelection == choice[0]) {
+    if ((playerSelection === rock && computerSelection === paper) || (playerSelection === paper && computerSelection === scissors) || (playerSelection === scissors && computerSelection === rock)) {
         console.log(`You lose, ${computerSelection} beats ${playerSelection}`)
-        return `lose`
-    } else if (playerSelection.toLowerCase() == "rock" && computerSelection == choice[2] || playerSelection.toLowerCase() == "paper" && computerSelection == choice[0] || playerSelection.toLowerCase() == "scissors" && computerSelection == choice[1] ) {
+        return false
+    } else if ((playerSelection === rock && computerSelection === scissors)|| (playerSelection === paper && computerSelection === rock) || (playerSelection === scissors && computerSelection === paper)) {
         console.log(`You win, ${playerSelection} beats ${computerSelection}`)
-        return `win`
-    } else if (playerSelection.toLowerCase() == computerSelection) {
-        console.log(`It's a tie`)
-    } else { console.log(`Error`)}
+        return true
+    } else if (playerSelection === computerSelection) {
+        console.log("It's a tie")
+        return undefined
+    } else {
+        console.log('Error')
+        return undefined
+    }
 }
 
 let playerScore = 0
 let computerScore = 0
 
 for (let i = 1; i <= 5; i++) {
-    let playerSelection = prompt(`Choose your move`).toLowerCase()
+    let playerSelection = prompt('Choose your move').toLowerCase()
     let computerSelection = getComputerChoice()
     const result = gameRound(playerSelection, computerSelection)
 
-    if (result == `win`) {
+    if (result == true) {
         playerScore++
-    } else if (result == `lose`) {
+    } else if (result == false) {
         computerScore++
     }
 }
 
 if (playerScore > computerScore) {
-    console.log(`Player is the winner`)
+    console.log('Player is the winner')
 } else if (playerScore < computerScore) {
-    console.log(`Computer is the winner`)
-} else { console.log(`It's a tie`) }
+    console.log('Computer is the winner')
+} else { console.log("It's a tie") }
 
 
