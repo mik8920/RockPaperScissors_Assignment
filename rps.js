31ea565a3d1fb3045be55f43567035754621a57f
mik8920 <daal.mikaela@gmail.com>
2024-01-18 17:33:08 +0200

    small changes

--- rps.js
+++ rps.js
@@ -1,49 +1,59 @@
-const rockButton = document.querySelector('#btnR')
-const paperButton = document.querySelector('#btnP')
-const scissorsButton = document.querySelector('#btnS')
+const rockButton = document.querySelector("#btnR");
+const paperButton = document.querySelector("#btnP");
+const scissorsButton = document.querySelector("#btnS");
 
-const choice = [`rock`, `paper`, `scissors`]
+const buttons = () => {
+  btnR.addEventListener("click", () => {
+    gameRound(playerSelection, computerSelection);
+  });
+  btnP.addEventListener("click", () => {
+    gameRound(playerSelection, computerSelection);
+    computerSelection = getComputerChoice();
+  });
+  btnS.addEventListener("click", () => {
+    gameRound(playerSelection, computerSelection);
+  });
+};
+
+const rock = "rock";
+const paper = "paper";
+const scissors = "scissors";
+
+const choice = [rock, paper, scissors];
 
 const getComputerChoice = () => {
-    const randomNumber = Math.floor(Math.random() * choice.length)
-    return choice[randomNumber]
-}
+  const randomNumber = Math.floor(Math.random() * choice.length);
+  return choice[randomNumber];
+};
 
-let computerSelection = getComputerChoice()
-/* let playerSelection = prompt(`Choose your move`).toLowerCase()
- */
+let computerSelection = getComputerChoice();
+let playerSelection = buttons();
 
 const gameRound = (playerSelection, computerSelection) => {
-
-    if (playerSelection === `rock` && computerSelection === choice[1] || playerSelection === `paper` && computerSelection === choice[2] || playerSelection === `scissors` && computerSelection === choice[0]) {
-        console.log(`You lose, ${computerSelection} beats ${playerSelection}`)
-        return `lose`
-    } else if (playerSelection === `rock` && computerSelection === choice[2] || playerSelection === `paper` && computerSelection === choice[0] || playerSelection === `scissors` && computerSelection === choice[1]) {
-        console.log(`You win, ${playerSelection} beats ${computerSelection}`)
-        return `win`
-    } else if (playerSelection === computerSelection) {
-        console.log(`It's a tie`)
-    } else { console.log(`Error`) }
-}
-
+  if (
+    (playerSelection === rock && computerSelection === paper) ||
+    (playerSelection === paper && computerSelection === scissors) ||
+    (playerSelection === scissors && computerSelection === rock)
+  ) {
+    console.log(`You lose, ${computerSelection} beats ${playerSelection}`);
+    return false;
+  } else if (
+    (playerSelection === rock && computerSelection === scissors) ||
+    (playerSelection === paper && computerSelection === rock) ||
+    (playerSelection === scissors && computerSelection === paper)
+  ) {
+    console.log(`You win, ${playerSelection} beats ${computerSelection}`);
+    return true;
+  } else if (playerSelection === computerSelection) {
+    console.log("It's a tie");
+    return undefined;
+  } else {
+    console.log("Error");
+    return undefined;
+  }
+};
 
 /*Add an event listener that calls gameRound function with the correct playerSelection every time a button is clicked.*/
-
-btnR.addEventListener('click', () => {
-    gameRound(playerSelection, computerSelection)
-})
-btnP.addEventListener('click', () => {
-    gameRound(playerSelection, computerSelection)
-    computerSelection = getComputerChoice()
-})
-btnS.addEventListener('click', () => {
-    gameRound(playerSelection, computerSelection)
-})
-
-
-
-
-
 
 /* let playerScore = 0
 let computerScore = 0
@@ -65,5 +75,3 @@ if (playerScore > computerScore) {
 } else if (playerScore < computerScore) {
     console.log(`Computer is the winner`)
 } else { console.log(`It's a tie`) } */
-
-
