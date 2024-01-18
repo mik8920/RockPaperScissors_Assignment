477c590c05ef1d1dbd5f8402351bab7465ac4217
mik8920 <daal.mikaela@gmail.com>
2024-01-18 10:33:17 +0200

    Revisiting Rock Paper Scissors

--- rps.js
+++ rps.js
@@ -1,3 +1,6 @@
+const rockButton = document.querySelector('#btnR')
+const paperButton = document.querySelector('#btnP')
+const scissorsButton = document.querySelector('#btnS')
 
 const choice = [`rock`, `paper`, `scissors`]
 
@@ -6,20 +9,43 @@ const getComputerChoice = () => {
     return choice[randomNumber]
 }
 
+let computerSelection = getComputerChoice()
+/* let playerSelection = prompt(`Choose your move`).toLowerCase()
+ */
+
 const gameRound = (playerSelection, computerSelection) => {
 
-    if (playerSelection==`rock` && computerSelection==choice[1] || playerSelection.toLowerCase() == "paper" && computerSelection == choice[2] || playerSelection.toLowerCase() == "scissors" && computerSelection == choice[0]) {
+    if (playerSelection === `rock` && computerSelection === choice[1] || playerSelection === `paper` && computerSelection === choice[2] || playerSelection === `scissors` && computerSelection === choice[0]) {
         console.log(`You lose, ${computerSelection} beats ${playerSelection}`)
         return `lose`
-    } else if (playerSelection.toLowerCase() == "rock" && computerSelection == choice[2] || playerSelection.toLowerCase() == "paper" && computerSelection == choice[0] || playerSelection.toLowerCase() == "scissors" && computerSelection == choice[1] ) {
+    } else if (playerSelection === `rock` && computerSelection === choice[2] || playerSelection === `paper` && computerSelection === choice[0] || playerSelection === `scissors` && computerSelection === choice[1]) {
         console.log(`You win, ${playerSelection} beats ${computerSelection}`)
         return `win`
-    } else if (playerSelection.toLowerCase() == computerSelection) {
+    } else if (playerSelection === computerSelection) {
         console.log(`It's a tie`)
-    } else { console.log(`Error`)}
+    } else { console.log(`Error`) }
 }
 
-let playerScore = 0
+
+/*Add an event listener that calls gameRound function with the correct playerSelection every time a button is clicked.*/
+
+btnR.addEventListener('click', () => {
+    gameRound(playerSelection, computerSelection)
+})
+btnP.addEventListener('click', () => {
+    gameRound(playerSelection, computerSelection)
+    computerSelection = getComputerChoice()
+})
+btnS.addEventListener('click', () => {
+    gameRound(playerSelection, computerSelection)
+})
+
+
+
+
+
+
+/* let playerScore = 0
 let computerScore = 0
 
 for (let i = 1; i <= 5; i++) {
@@ -38,6 +64,6 @@ if (playerScore > computerScore) {
     console.log(`Player is the winner`)
 } else if (playerScore < computerScore) {
     console.log(`Computer is the winner`)
-} else { console.log(`It's a tie`) }
+} else { console.log(`It's a tie`) } */
 
 
