1f211da7fd3413b3f43e6be0946fb93cb07a6b1b
mik8920 <daal.mikaela@gmail.com>
2024-01-20 00:52:43 +0200

    add css and js

--- rps.js
+++ rps.js
@@ -1,13 +1,15 @@
 const rockButton = document.getElementById("rockBtn");
 const paperButton = document.getElementById("paperBtn");
 const scissorsButton = document.getElementById("scissorsBtn");
-const newGameButton = document.getElementById("newGameBtn")
+const newGameButton = document.getElementById("newGameBtn");
 const result = document.querySelector(".result");
 const playerScoreMessage = document.querySelector(".playerScore");
 const computerScoreMessage = document.querySelector(".computerScore");
-
-
-let isOver = false;
+result.classList.add("result-style");
+playerScoreMessage.classList.add("score-style");
+computerScoreMessage.classList.add("score-style");
+newGameButton.classList.add("newGame-style");
+let gameOver = false;
 
 let rock = rockButton.textContent;
 let paper = paperButton.textContent;
@@ -23,7 +25,7 @@ let playerScore = 0;
 let computerScore = 0;
 
 const buttonClick = (e) => {
-  if (isOver) {
+  if (gameOver) {
     return;
   }
   const playerSelection = e.target.textContent;
@@ -32,18 +34,46 @@ const buttonClick = (e) => {
 };
 
 const newGameClick = (e) => {
-playerScore=0
-computerScore=0
-result.textContent=""
-computerScoreMessage.textContent=""
-playerScoreMessage.textContent=""
+  playerScore = 0;
+  computerScore = 0;
+  result.textContent = "";
+  computerScoreMessage.textContent = "";
+  playerScoreMessage.textContent = "";
+  playerScoreMessage.style.color = "white";
+  computerScoreMessage.style.color = "white";
+  gameOver = false;
+  buttonClick();
 };
 
+rockButton.addEventListener("mouseover", () => {
+  rockButton.style.cursor = "pointer";
+});
+rockButton.addEventListener("mouseout", () => {
+  rockButton.style.cursor = "default";
+});
+paperButton.addEventListener("mouseover", () => {
+  paperButton.style.cursor = "pointer";
+});
+paperButton.addEventListener("mouseout", () => {
+  paperButton.style.cursor = "default";
+});
+scissorsButton.addEventListener("mouseover", () => {
+  scissorsButton.style.cursor = "pointer";
+});
+scissorsButton.addEventListener("mouseout", () => {
+  scissorsButton.style.cursor = "default";
+});
+newGameButton.addEventListener("mouseover", () => {
+  newGameButton.style.cursor = "pointer";
+});
+newGameButton.addEventListener("mouseout", () => {
+  newGameButton.style.cursor = "default";
+});
 
 rockButton.addEventListener("click", buttonClick);
 paperButton.addEventListener("click", buttonClick);
 scissorsButton.addEventListener("click", buttonClick);
-newGameButton.addEventListener("click",newGameClick)
+newGameButton.addEventListener("click", newGameClick);
 
 const gameRound = (playerSelection, computerSelection) => {
   if (
@@ -51,12 +81,16 @@ const gameRound = (playerSelection, computerSelection) => {
     (playerSelection === paper && computerSelection === scissors) ||
     (playerSelection === scissors && computerSelection === rock)
   ) {
-    result.textContent = `You lose, ${computerSelection} beats ${playerSelection}`;
+    result.textContent = `You lose, ${computerSelection} beats ${playerSelection}!`;
     computerScore++;
-    computerScoreMessage.textContent = `Computer's score: ${computerScore}`;
+    computerScoreMessage.textContent = `Computer score: ${computerScore}`;
     if (computerScore === 5) {
-      isOver = true;
-      result.textContent = "Computer is the winner";
+      gameOver = true;
+      result.textContent = "Computer is the winner, game over!";
+      result.style.color = "gold";
+      result.style.fontWeight = "bold";
+      computerScoreMessage.style.color = "red";
+
       return;
     }
     return;
@@ -65,44 +99,21 @@ const gameRound = (playerSelection, computerSelection) => {
     (playerSelection === paper && computerSelection === rock) ||
     (playerSelection === scissors && computerSelection === paper)
   ) {
-    result.textContent = `You win, ${playerSelection} beats ${computerSelection}`;
+    result.textContent = `You win, ${playerSelection} beats ${computerSelection}!`;
     playerScore++;
-    playerScoreMessage.textContent = `Player's score: ${playerScore}`;
+    playerScoreMessage.textContent = `Player score: ${playerScore}`;
     if (playerScore === 5) {
-      isOver = true;
-      result.textContent = "Player is the winner";
+      gameOver = true;
+      result.textContent = "Player is the winner, congratulations!";
+      result.style.color = "gold";
+      result.style.fontWeight = "bold";
+      playerScoreMessage.style.color = "red";
+
       return;
     }
     return;
-  } else  {
-    result.textContent = "It's a tie";
+  } else {
+    result.textContent = `It's a tie, ${computerSelection} is equal to ${playerSelection}!`;
     return;
   }
 };
-
-/* Add a div for for results and change the console.log with DOM
- */
-/* do {
-  gameRound(playerSelection, computerSelection);
-} while (playerScore === 5 || computerScore === 5); */
-
-/* const result = gameRound(playerSelection, computerSelection); */
-
-/* for (let i = 1; i <= 5; i++) {
-
-
-  if (result === true) {
-    playerScore++;
-  } else if (result === false) {
-    computerScore++;
-  }
-} */
-
-/* if (playerScore > computerScore) {
-  console.log("Player is the winner");
-} else if (playerScore < computerScore) {
-  console.log("Computer is the winner");
-} else {
-  console.log("It's a tie");
-}
- */
